9452f55389f220e9bdb393c58c198ff0c1690d39
jamessenior642 <jvsenior02@gmail.com>
2024-12-28 16:08:51 -0500

    4.2 restructured blogs app with separate modules

--- index.js
+++ index.js
@@ -1,42 +1,51 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
 
-const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb+srv://seniorj:fullstackopen@cluster0.rjwlj.mongodb.net/blogList?retryWrites=true&w=majority&appName=Cluster0'
-mongoose.connect(mongoUrl)
+// const express = require('express')
+// const app = express()
+// const cors = require('cors')
+// const mongoose = require('mongoose')
 
-app.use(cors())
-app.use(express.json())
+// const blogSchema = new mongoose.Schema({
+//   title: String,
+//   author: String,
+//   url: String,
+//   likes: Number
+// })
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+// const Blog = mongoose.model('Blog', blogSchema)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+// const mongoUrl = 'mongodb+srv://seniorj:fullstackopen@cluster0.rjwlj.mongodb.net/blogList?retryWrites=true&w=majority&appName=Cluster0'
+// mongoose.connect(mongoUrl)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+// app.use(cors())
+// app.use(express.json())
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+// app.get('/api/blogs', (request, response) => {
+//   Blog
+//     .find({})
+//     .then(blogs => {
+//       response.json(blogs)
+//     })
+// })
+
+// app.post('/api/blogs', (request, response) => {
+//   const blog = new Blog(request.body)
+
+//   blog
+//     .save()
+//     .then(result => {
+//       response.status(201).json(result)
+//     })
+// })
+
+// const PORT = 3003
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`)
+// })
